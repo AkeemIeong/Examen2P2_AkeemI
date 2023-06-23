3e16f33b416e57445072b345797118d06752bacd
Akeem Ieong Ham <akeemieong@gmail.com>
2023-06-23 13:07:16 -0600

    primer commit

---
+++ src/examen2p2_akeemi/Examen2P2_AkeemI.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen2p2_akeemi;
+
+/**
+ *
+ * @author SURFACEB2I7
+ */
+public class Examen2P2_AkeemI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
